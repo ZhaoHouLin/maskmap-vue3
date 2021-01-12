72ff1ac4820bd3207c1bd66b667029889f7d111c
Raiden Lin <rodes5292@gmail.com>
2021-01-12 11:56:17 +0800

    edit map maskinfo style

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -116,43 +116,143 @@ export default {
       l-marker( :key='key' v-if='item.properties.county === selectList.city && item.properties.town === selectList.area' :lat-lng='[item.geometry.coordinates[1],item.geometry.coordinates[0]]' @click='reCenter(item.geometry.coordinates)' )
         l-icon(:icon-url="$route.params.id===item.properties.name?actIconUrl:distance(item.geometry.coordinates[1],item.geometry.coordinates[0])<1?nearIconUrl:iconUrl" :icon-size="iconSize" )
         l-popup
-          h2 {{item.properties.name}}
-          h3 成人口罩: {{item.properties.mask_adult?item.properties.mask_adult+'個':'未取得資料'}}
-          h3 兒童口罩: {{item.properties.mask_child?item.properties.mask_child+'個':'未取得資料'}}
-          h3 
-            | 地址: 
-            a(:href='`https://www.google.com.tw/maps/place/${item.properties.address}`' target='_blank' title='Google Map') {{ item.properties.address }} 
-            h4 距離: {{distance(item.geometry.coordinates[1],item.geometry.coordinates[0])* 1000 + '公尺'}}
-          l-tooltip
-            h3 成人: {{item.properties.mask_adult?item.properties.mask_adult+'個':'未取得資料'}}
-            h3 兒童: {{item.properties.mask_child?item.properties.mask_child+'個':'未取得資料'}}
-            h3 距離: {{distance(item.geometry.coordinates[1],item.geometry.coordinates[0])* 1000 + '公尺'}} 
+          .title
+            i.fas.fa-pills
+            h1 {{item.properties.name}}
+          .mask-quantity
+            .mask-adult
+              h4 成人口罩 
+              h3 {{item.properties.mask_adult?item.properties.mask_adult+'個':'未取得資料'}}
+            .mask-child
+              h4 兒童口罩
+              h3 {{item.properties.mask_child?item.properties.mask_child+'個':'未取得資料'}}
+          .info 
+            h2 {{item.properties.phone}} 
+              //- h4 {{item.properties.phone}}
+            h2
+              a(:href='`https://www.google.com.tw/maps/place/${item.properties.address}`' target='_blank' title='Google Map') {{ item.properties.address }} 
+            .distance
+              h2 距離您 
+              h3(:class='[{"green": distance(item.geometry.coordinates[1],item.geometry.coordinates[0])* 1000<1000 }]') {{distance(item.geometry.coordinates[1],item.geometry.coordinates[0])* 1000 }} 
+              h2 公尺
+
+            h2 備註: 
+              h4 {{item.properties.note}}
+            h4 更新時間: {{item.properties.updated}}
             
     l-marker(:lat-lng='[userCoordinatesData.latitude,userCoordinatesData.longitude]' @click='reCenter([userCoordinatesData.longitude,userCoordinatesData.latitude])')
       l-icon(:icon-url='`https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png`' :icon-size="iconSize")
       l-tooltip.tooltip(:options="{interactive: true,permanent: true}" ) 你在這
 
     .lMarker(v-for='(item,key) in nearPharmacyData')
-      l-marker(:key='key' :lat-lng='[item.geometry.coordinates[1],item.geometry.coordinates[0]]' @click='reCenter(item.geometry.coordinates)' )
+      l-marker(:key='key' :lat-lng='[item.geometry.coordinates[1],item.geometry.coordinates[0]]' @click='reCenter(item.geometry.coordinates)')
         l-icon(:icon-url="`https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png`" :icon-size="iconSize" )
         l-popup
-          h2 {{item.properties.name}}
-          h3 成人口罩: {{item.properties.mask_adult?item.properties.mask_adult+'個':'未取得資料'}}
-          h3 兒童口罩: {{item.properties.mask_child?item.properties.mask_child+'個':'未取得資料'}}
-          h3 
-            | 地址: 
-            a(:href='`https://www.google.com.tw/maps/place/${item.properties.address}`' target='_blank' title='Google Map') {{ item.properties.address }} 
-            h4 距離: {{distance(item.geometry.coordinates[1],item.geometry.coordinates[0])* 1000 + '公尺'}}
+          .title
+            i.fas.fa-pills
+            h1 {{item.properties.name}}
+          .mask-quantity
+            .mask-adult
+              h4 成人口罩 
+              h3 {{item.properties.mask_adult?item.properties.mask_adult+'個':'未取得資料'}}
+            .mask-child
+              h4 兒童口罩
+              h3 {{item.properties.mask_child?item.properties.mask_child+'個':'未取得資料'}}
+          .info 
+            h2 {{item.properties.phone}} 
+            h2
+              a(:href='`https://www.google.com.tw/maps/place/${item.properties.address}`' target='_blank' title='Google Map') {{ item.properties.address }} 
+            .distance
+              h2 距離您 
+              h3(:class='[{"green": distance(item.geometry.coordinates[1],item.geometry.coordinates[0])* 1000<1000 }]') {{distance(item.geometry.coordinates[1],item.geometry.coordinates[0])* 1000 }} 
+              h2 公尺
+
+            h2 備註: 
+              h4 {{item.properties.note}}
+            h4 更新時間: {{item.properties.updated}}
+          
           l-tooltip
             h3 成人: {{item.properties.mask_adult?item.properties.mask_adult+'個':'未取得資料'}}
             h3 兒童: {{item.properties.mask_child?item.properties.mask_child+'個':'未取得資料'}}
             h3 距離: {{distance(item.geometry.coordinates[1],item.geometry.coordinates[0])* 1000 + '公尺'}} 
+
 </template>
 
-<style lang='stylus' scoped>
+<style lang='stylus'>
 @import '../css/style.styl'
+
 .map
   flexCenter()
   size()
+
+a.leaflet-popup-close-button
+  font-size 32px
+  color #000
+
+.leaflet-popup-content-wrapper
+  size(auto,auto)
+  border-radius 32px
+  padding 0
+  padding-bottom 8px
+  .leaflet-popup-content
+    border-radius 32px
+    margin 0
+ 
+.title
+  border-radius 16px 16px 0 0
+  flexCenter(flex-start,center,)
+  size(100%,auto)
+  background-color #1bd641
+  padding 8px 16px
+  i 
+    font-size 16px
+    color #fff
+  h1
+    margin-left 8px
+    color #fff
+    
+.mask-quantity
+  flexCenter()
+  .mask-adult,.mask-child
+    size(100%,auto)
+    padding 12px
+    flexCenter(flex-start,center,column)
+    h4
+      font-size 18px
+    h3
+      font-size 24px
+      color #fff
+      
+  .mask-adult
+    background-color #30BCED
+  .mask-child 
+    background-color #ffcd3a
+
+.info
+  margin-top 8px
+  padding 0 16px
+  h2
+    margin 4px 0
+    a
+      font-size 16px
+      font-weight 600
+    h4
+      display inline-block
+      font-size 16px
+      font-weight 500
+    h2
+      display inline-block
+  .distance
+    flexCenter(flex-start,flex-start,)
+    line-height 36px
+    h2,h3
+      margin-right 4px
+    h2 
+      font-weight 500
+    h3
+      color red
+      font-size 24px
+      &.green
+        color #1bd641
 
 </style>
